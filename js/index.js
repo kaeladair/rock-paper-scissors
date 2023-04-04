6b83762b4a3f684b77a41b270c1ad4be8fcc93c6
Kael Adair <kja75810@gmail.com>
2023-04-04 16:56:27 -0500

    Create computer choice function

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 
+// Pick a random choice from rock, paper, or scissors
+function getComputerChoice() {
+    let choices = ["Rock", "Paper", "Scissors"];
+    let choice = Math.floor(Math.random() * 2);
+    return choices[choice];
+}
+
