b8662a3542dd485b176960c59593c9c4ec98c6fd
Kael Adair <kja75810@gmail.com>
2023-04-04 17:10:35 -0500

    Implement playRound function

--- js/index.js
+++ js/index.js
@@ -6,3 +6,29 @@ function getComputerChoice() {
     return choices[choice];
 }
 
+// Play a single round of rock, paper, scissors
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection){
+        return "It's a tie!";
+    }
+
+    if (playerSelection === "Rock"){
+        if (computerSelection === "Paper"){
+            return "You Lose! Paper beats Rock.";
+        } else if (computerSelection === "Scissors"){
+            return "You Win! Rock beats Scissors";
+        }
+    } else if (playerSelection === "Paper"){
+        if (computerSelection === "Rock"){
+            return "You Win! Paper beats Rock!";
+        } else if (computerSelection === "Scissors"){
+            return "You Lose! Scissors beats Paper.";
+        }
+    } else {
+        if (computerSelection === "Paper"){
+            return "You Win! Scissors beats Paper.";
+        } else if (computerSelection === "Rock"){
+            return "You Lose! Rock beats Scissors";
+        }
+    }
+}
