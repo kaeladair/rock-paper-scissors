77a17ae99b7d6c953a22a68e75706513e00e02e0
Kael Adair <kja75810@gmail.com>
2023-04-05 16:37:14 -0500

    Display result as text in div

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 
+const div = document.querySelector('.output');
+
 // Pick a random choice from rock, paper, or scissors
 function getComputerChoice() {
     let choices = ["Rock", "Paper", "Scissors"];
@@ -10,32 +12,32 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === computerSelection) {
-        alert("It's a tie!");
+        div.textContent = "It's a tie!";
         return 0;
     }
 
     if (playerSelection === "Rock") {
         if (computerSelection === "Paper") {
-            alert( "You Lose! Paper beats Rock.");
+            div.textContent = "You Lose! Paper beats Rock."            
             return 1;
         } else if (computerSelection === "Scissors") {
-            alert( "You Win! Rock beats Scissors");
+            div.textContent = "You Win! Rock beats Scissors.";
             return 2;
         }
     } else if (playerSelection === "Paper") {
         if (computerSelection === "Rock") {
-            alert( "You Win! Paper beats Rock!");
+            div.textContent = "You Win! Paper beats Rock!";
             return 2;
         } else if (computerSelection === "Scissors") {
-            alert("You Lose! Scissors beats Paper.");
+            div.textContent = "You Lose! Scissors beats Paper.";
             return 1;
         }
     } else {
         if (computerSelection === "Paper") {
-            alert("You Win! Scissors beats Paper.");
+            div.textContent = "You Win! Scissors beats Paper.";
             return 2;
         } else if (computerSelection === "Rock") {
-            alert("You Lose! Rock beats Scissors");
+            div.textContent = "You Lose! Rock beats Scissors.";
             return 1;
         }
     }
